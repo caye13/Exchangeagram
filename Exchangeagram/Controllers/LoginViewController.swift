7f5a7e3dbddc809ff53a8a3be2bb1f1521567ad7
caye <caye1300@gmail.com>
2020-08-07 14:17:21 -0700

    log in

--- Exchangeagram/Controllers/LoginViewController.swift
+++ Exchangeagram/Controllers/LoginViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        print("login button tapped")
     }
     
 }
