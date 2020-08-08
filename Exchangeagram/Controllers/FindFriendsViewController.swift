58fe762e50112f2825bd918b11ec636c2266f012
caye <caye1300@gmail.com>
2020-08-07 21:20:13 -0700

    new uiviewcontrollers

---
+++ Exchangeagram/Controllers/FindFriendsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FindFriendsViewController.swift
+//  Exchangeagram
+//
+//  Created by Ck2 Jedi on 8/7/20.
+//  Copyright © 2020 caye. All rights reserved.
+//
+
+import UIKit
+
+class FindFriendsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Exchangeagram/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Exchangeagram
+//
+//  Created by Ck2 Jedi on 8/7/20.
+//  Copyright © 2020 caye. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
