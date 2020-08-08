5bcbeee9a3939601baf72de9903d168b93e306ab
caye <caye1300@gmail.com>
2020-08-07 23:10:46 -0700

    posts from firebase

--- Exchangeagram/Controllers/HomeViewController.swift
+++ Exchangeagram/Controllers/HomeViewController.swift
@@ -11,9 +11,29 @@ import UIKit
 class HomeViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
-    
+   
+    var posts = [Post]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        UserService.posts(for: User.current) { (posts) in
+            self.posts = posts
+            self.tableView.reloadData()
+        }
     }
     
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath)
+        cell.backgroundColor = .red
+
+        return cell
+    }
 }
