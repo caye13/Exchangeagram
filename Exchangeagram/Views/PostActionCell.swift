1dd10e600bf4511d29b6026126f0ec4056f707c4
caye <caye1300@gmail.com>
2020-08-08 08:47:13 -0700

    like photos

---
+++ Exchangeagram/Views/PostActionCell.swift
@@ -0,0 +1,18 @@
+//
+//  PostActionCell.swift
+//  Exchangeagram
+//
+//  Created by Ck2 Jedi on 8/8/20.
+//  Copyright © 2020 caye. All rights reserved.
+//
+
+import UIKit
+
+class PostActionCell: UITableViewCell {
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+}
---
+++ Exchangeagram/Views/PostHeaderCell.swift
@@ -0,0 +1,22 @@
+//
+//  PostHeaderCell.swift
+//  Exchangeagram
+//
+//  Created by Ck2 Jedi on 8/8/20.
+//  Copyright © 2020 caye. All rights reserved.
+//
+
+import UIKit
+
+class PostHeaderCell: UITableViewCell {
+
+    @IBOutlet var usernameLable: UILabel!
+   
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    @IBAction func optionsButtonTapped(_ sender: UIButton) {
+    }
+    
+}
